aac794542ad83dc19a92e11285d74c59f7ccf9b1
Alexey Kuznetsov <a.kuznetsov@okft.io>
2023-04-19 17:01:03 +0500

    Init gem

---
+++ spec/firekassa_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Firekassa do
+  it "has a version number" do
+    expect(Firekassa::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
