886246e85007255c0e76ddf3d2eb5a433a33d788
Alexey Kuznetsov <a.kuznetsov@okft.io>
2023-04-19 17:12:26 +0500

    Remove failng test to see green CI once

--- spec/firekassa_spec.rb
+++ spec/firekassa_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Firekassa do
     expect(Firekassa::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "just should be green" do
+    expect(true).to eq(true)
   end
 end
